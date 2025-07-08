e577e45a869581938600ba67df552989f82ebeb9
Neel1992004 <pneel1924@gmail.com>
2025-07-08 17:06:01 +0530

    Added full MongoDB integration with CRUD using Mongoose

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,45 +1,64 @@
-const users = require("../data/users")  // access dummy data
+// const users = require("../data/users")  // access dummy data
+const User = require('../models/User')
 
 // get all the users
-const getUsers = (req, res) => {
+const getUsers = async (req, res) => {
+    const users = await User.find()
     res.json(users);
 }
 
 // create new users
-const createUser = (req, res) => {
+const createUser = async (req, res) => {
     const { name } = req.body;  // get name from body
-    const newUser = {
-        id: Date.now(),
-        name
-    }
-    users.push(newUser);  // add newUser into users(dummy data)
-    res.status(201).json({ message: "User created", user: newUser })
+    // for dummy data
+    // const newUser = {   
+    //     id: Date.now(),
+    //     name
+    // }
+    // users.push(newUser);  // add newUser into users(dummy data)
+    // res.status(201).json({ message: "User created", user: newUser })
+
+
+    // from database
+    const user = await User.create({ name })
+    res.status(201).json(user)
 }
 
 // upate user
-const updateUser = (req, res) => {
+const updateUser = async (req, res) => {
     const { id } = req.params
     const { name } = req.body
 
-    const user = users.find(u => u.id == id)
-    if (!user) {
-        return res.status(404).json({ message: "User not found" })
-    }
-    user.name = name
+    //for dummy data
+    // const user = users.find(u => u.id == id)
+    // if (!user) {
+    //     return res.status(404).json({ message: "User not found" })
+    // }
+    // user.name = name
+
+    // from database
+    const user = await User.findByIdAndUpdate(id, { name }, { new: true })
+    if (!user) return res.status(204).json({ message: "User not found" })
     res.json({ message: "User updated", user })
 }
 
 // delete user
-const deleteUser = (req, res) => {
+const deleteUser = async (req, res) => {
     const { id } = req.params
-    const index = users.findIndex(u => u.id == id)
 
-    if (index == -1) {
-        return res.status(404).json({ message: "User not found" })
-    }
+    // for dummy data
+    // const index = users.findIndex(u => u.id == id)
+    // if (index == -1) {
+    //     return res.status(404).json({ message: "User not found" })
+    // }
+    // const deletedUser = users.splice(index, 1)
+    // res.json({ message: "User deleted", user: deletedUser[0] })
 
-    const deletedUser = users.splice(index, 1)
-    res.json({ message: "User deleted", user: deletedUser[0] })
+    // from database
+
+    const user = await User.findByIdAndDelete(id)
+    if (!user) return res.status(404).json({ message: "User not found" })
+    res.json({ message: "User deleted", user })
 }
 
 
--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express")
 const userRoutes = require("./routes/userRoutes")
+const { config } = require("dotenv")
+const connectDB = require("./config/db")
+const dotenv = require('dotenv')
+
+dotenv.config()
+connectDB()
 
 const app = express()
-const PORT = 5000
-
 app.use(express.json()) // body parser
+
 app.use("/api/users", userRoutes)
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`)
+app.listen(process.env.PORT, () => {
+    console.log(`Server running at http://localhost:${process.env.PORT}`)
 })
