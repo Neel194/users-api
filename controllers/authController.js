b748dfb4c856a28b6d6a4b1c62a4dd94fb630b6f
Neel1992004 <pneel1924@gmail.com>
2025-07-12 19:52:56 +0530

    Added login route with JWT token & bcrypt password check

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require("../models/User")
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res, next) => {
     try {
@@ -37,4 +38,44 @@ const register = async (req, res, next) => {
     }
 }
 
-module.exports = { register };
+const login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body
+
+        // check if user exist
+        const user = await User.findOne({ email })
+        if (!user) {
+            const error = new Error("Invalid email or password")
+            error.statusCode = 401
+            return next(error)
+        }
+
+        //compare password
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) {
+            const error = new Error("Invalid email or password")
+            error.statusCode = 401
+            return next(error)
+        }
+
+        //genreate token
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "7d"
+        })
+
+        res.status(200).json({
+            success: true,
+            message: "Login successfull",
+            token,
+            user: {
+                _id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        })
+    } catch (err) {
+        next(err)
+    }
+}
+
+module.exports = { register, login };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,11 @@ const router = express.Router()
 const { register } = require("../controllers/authController")
 const { validate } = require("../middlewares/validateZod")
 const { registerSchema } = require("../validations/authValidation")
+const { loginSchema } = require("../validations/authValidation")
+const { login } = require("../controllers/authController")
+
 
 router.post("/register", validate(registerSchema), register)
+router.post("/login", validate(loginSchema), login)
 
 module.exports = router
